993a5c802dffd8be99fe41436e8a673bef1714d2
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-24 19:37:42 +0300

    updated package.json

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -9,7 +9,6 @@ function MobileNav() {
           <NavLink to="/" activeclassname='active'><h5 className="flex">⭐ Popular</h5></NavLink>
           <NavLink to="/Wellness" activeclassname='active'><h5>Wellness</h5></NavLink>
           <NavLink to="/NightLife" activeclassname='active'><h5>Night Life</h5></NavLink>
-          {/* <a href="/Tours"><h5>VIP</h5></a> */}
           <NavLink to="/VIP" activeclassname='active'><h5>VIP</h5></NavLink>
           <NavLink to="/Activities" activeclassname='active'><h5>Tours + Activities</h5></NavLink>
       </div>
