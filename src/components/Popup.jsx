639ab808198d837afdec1f48f264273469989635
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-10-02 16:17:20 +0300

    update navigation

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,9 +3,10 @@ import "./css/popup.css"
 
 const Popup = ({ setIsOpen }) => {
   return (
-    <div className="slack">
-      <h5 style={{color:'orange'}}>24/7 deliveries coming soon </h5><div className='closer-con' onClick={() => setIsOpen(false)}><h5 className="closer" >+</h5></div>
-    </div>
+    <></>
+    // <div className="slack">
+    //   <h5 style={{color:'orange'}}>24/7 deliveries coming soon </h5><div className='closer-con' onClick={() => setIsOpen(false)}><h5 className="closer" >+</h5></div>
+    // </div>
   )
 }
 
